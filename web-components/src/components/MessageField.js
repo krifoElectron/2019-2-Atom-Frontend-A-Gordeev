b6110708c8bacb3b2e1d237a4d56466a8c99617f
krifoElectron <ars2d2@yandex.ru>
2019-10-23 00:53:39 +0300

    refactoring, add hat. со скролингом пока проблема, когда сообщений больше, чем высота. под шапкой прячутся самые верхние сообщения

--- web-components/src/components/MessageField.js
+++ web-components/src/components/MessageField.js
@@ -3,6 +3,8 @@ template.innerHTML = `
   <style>
     form-input {
       width: 100%;
+      display: flex;
+      flex-direction: row;
     }
     
     .result {
@@ -23,7 +25,7 @@ template.innerHTML = `
       justify-content: flex-end;
     }
     
-    message-mess {
+    message-element {
       background-color: #ffdfcf; /*#ffc0cb;*/
       border-radius: 5px;
       margin: 10px;
@@ -36,7 +38,7 @@ template.innerHTML = `
       max-width: 60%;
     }
 
-    message-mess::after {
+    message-element::after {
       content: '';
       position: absolute;
       right: -12px;
@@ -44,12 +46,19 @@ template.innerHTML = `
       border: 7px solid transparent;
       border-left: 9px solid #ffdfcf;
     }
-    form-input {
-      display: flex;
-      flex-direction: row;
-    }
-    </style>
 
+    .hat {
+      width: 100%;
+      background: #f19cbb;
+      height: 100px;
+      position: fixed;
+      top: 0;
+      z-index: 1;
+    }
+    
+    </style>
+  
+  <div class="hat"></div>
   <div class="container">
     <form class="mess-form">
       <form-input name="message-text" class="message-input" placeholder="Введите сообщеине"></form-input>
@@ -58,86 +67,100 @@ template.innerHTML = `
 `;
 
 class MessageField extends HTMLElement {
-constructor () {
-  super();
-  this._shadowRoot = this.attachShadow({ mode: 'open' });
-  this._shadowRoot.appendChild(template.content.cloneNode(true));
-  this.$form = this._shadowRoot.querySelector('form');
-  this.$input = this._shadowRoot.querySelector('form-input');
-  this.$form.addEventListener('submit', this._onSubmit.bind(this));
-  this.$form.addEventListener('keypress', this._onKeyPress.bind(this));
-  let i = 1;
-  while (localStorage.getItem(i)) {
-    const message =JSON.parse(localStorage.getItem(i));
-    const mess2 = document.createElement('message-mess');
-    const messageTime = document.createElement('span');
-    messageTime.setAttribute('slot', 'time');
-    messageTime.textContent = message.time;
-    mess2.appendChild(messageTime);
-
-    const messageText = document.createElement('span');
-    messageText.setAttribute('slot', 'message');
-    messageText.textContent = message.text;
-    mess2.appendChild(messageText);
-
-    this.$form.insertAdjacentElement('beforebegin', mess2);
-
-    this._mess_id = i;
-    i += 1;
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+    this.$form = this._shadowRoot.querySelector('form');
+    this.$input = this._shadowRoot.querySelector('form-input');
+    this.$form.addEventListener('submit', this._onSubmit.bind(this));
+    this.$form.addEventListener('keypress', this._onKeyPress.bind(this));
+    if (localStorage.getItem('messenegerState')) {
+      const message = JSON.parse(localStorage.getItem('messenegerState'));
+      message.chats[0].forEach((element) => {
+        this._createMessageElement(element);
+      });
+    }
   }
-}
 
   _getIdMess() {
-    if ( this._mess_id) {
+    if (this._mess_id) {
       this._mess_id += 1;
     } else {
-      this._mess_id=1;
+      this._mess_id = 1;
     }
     return this._mess_id;
   }
 
-  _onSubmit (event) {
-    event.preventDefault();
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const formatedTime = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
-    const message = {text: this.$input.value,
-                    time: formatedTime,
-                    isRead: true};
-
-    // const mess = document.createElement('div');
-    // mess.classList.add('mess-element');
-    // mess.innerHTML = this.$input.value;
-    // this.$form.insertAdjacentElement('beforebegin', mess);
-
-    const mess2 = document.createElement('message-mess');
-    // mess2.classList.add('mess-element');
-    // mess2.innerHTML = this.$input.value;
-    
-    const messageText = document.createElement('span');
-    messageText.setAttribute('slot', 'message');
-    messageText.textContent = this.$input.value;
-    mess2.appendChild(messageText);
-
-    const messageTime = document.createElement('span');
-    messageTime.setAttribute('slot', 'time');
-    messageTime.textContent = message.time;
-    mess2.appendChild(messageTime);
-
-    this.$form.insertAdjacentElement('beforebegin', mess2);
-
-    const idM =String(this._getIdMess());
-    localStorage.setItem(idM, JSON.stringify(message));
-
-    this.$input.scrollIntoView(false);
+  _onSubmit(event) {
+    if (this.$input.value === '') {
+      return;
     }
 
-  _onKeyPress (event) {
+    event.preventDefault();
+
+    let message = JSON.parse(localStorage.getItem('messenegerState'));
+
+    const { value } = this.$input;
+    const date = new Date();
+
+    if (!message) {
+      message = {
+        chats: {
+          '0': [
+            {
+              sender: 'a',
+              recipient: 'b',
+              text: value,
+              date,
+              isRead: true,
+            },
+          ],
+        },
+      };
+    } else {
+      message.chats['0'].push({
+        sender: 'a',
+        recipient: 'b',
+        text: value,
+        date,
+      });
+    }
+    const len = message.chats[0].length;
+    this._createMessageElement(message.chats[0][len - 1]);
+
+    localStorage.setItem('messenegerState', JSON.stringify(message));
+
+    this.$input.value = '';
+    this.$input.scrollIntoView(false);
+  }
+
+  _createMessageElement({ text, date }) {
+    const normDate = new Date(Date.parse(date));
+    const hours = normDate.getHours();
+    const minutes = normDate.getMinutes();
+    const formatedTime = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+
+    const messageElement = document.createElement('message-element');
+
+    const messageText = document.createElement('span');
+    messageText.setAttribute('slot', 'message');
+    messageText.textContent = text;
+    messageElement.appendChild(messageText);
+
+    const messageDate = document.createElement('span');
+    messageDate.setAttribute('slot', 'date');
+    messageDate.textContent = formatedTime;
+    messageElement.appendChild(messageDate);
+
+    this.$form.insertAdjacentElement('beforebegin', messageElement);
+  }
+
+  _onKeyPress(event) {
     if (event.keyCode === 13) {
       this.$form.dispatchEvent(new Event('submit'));
     }
   }
 }
 
-customElements.define('message-form', MessageField);
+customElements.define('message-container', MessageField);
