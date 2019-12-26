64ab2bd0e469dd089c4ea70451a143fca164eebf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-26 19:06:51 +0000

    Deploy krifoElectron/2019-2-Atom-Frontend-A-Gordeev to github.com/krifoElectron/2019-2-Atom-Frontend-A-Gordeev.git:gh-pages

---
+++ precache-manifest.4ac256383e647f6468199e4f7684334a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788071562ac33a48c6de33b0a96fab2c",
+    "url": "/2019-2-Atom-Frontend-A-Gordeev/index.html"
+  },
+  {
+    "revision": "3b0f7dcc727eb4213de8",
+    "url": "/2019-2-Atom-Frontend-A-Gordeev/static/css/main.af98f9e1.chunk.css"
+  },
+  {
+    "revision": "a926488b5695edc4c7ec",
+    "url": "/2019-2-Atom-Frontend-A-Gordeev/static/js/2.e09a0d80.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/2019-2-Atom-Frontend-A-Gordeev/static/js/2.e09a0d80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b0f7dcc727eb4213de8",
+    "url": "/2019-2-Atom-Frontend-A-Gordeev/static/js/main.c1cca3e7.chunk.js"
+  },
+  {
+    "revision": "fc6851c96ccd32a173a7",
+    "url": "/2019-2-Atom-Frontend-A-Gordeev/static/js/runtime-main.28fdf60b.js"
+  },
+  {
+    "revision": "ec307a3b32902360d8a7489715c4cf4e",
+    "url": "/2019-2-Atom-Frontend-A-Gordeev/static/media/newCity.ec307a3b.svg"
+  }
+]);
